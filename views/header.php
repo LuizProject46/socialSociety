42f7ba3f3f28cc2f567581858ffb1cc8ab4c57f3
LuizGustavoMoreira <luiz@vpeventos.com>
2020-08-02 23:57:04 -0300

    first commit

--- views/header.php
+++ views/header.php
@@ -10,7 +10,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-	<link href="../../assets/css/material-dashboard.min.css?v=2.1.2" type="text/css" rel="stylesheet" />
+	<link href="../assets/css/material-dashboard.min.css?v=2.1.2" type="text/css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.min.css"/>
 </head>
 <script>
